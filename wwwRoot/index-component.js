ef297d51008a78e971cf9a23a834d48524711fdb
Camille <camille@fadeuilhe.com>
2020-11-03 16:34:02 +0100

    Création d'un composant bouton

---
+++ wwwRoot/index-component.js
@@ -0,0 +1,21 @@
+// Nous créons une application
+const app = Vue.createApp({
+    methods: {
+        buttonClick: function() {
+            console.log('test');
+        }
+    }
+})
+app.component('icon-button', {
+    props: {
+        title: String,
+        icon: String,
+        position: String,
+        type: String
+    },
+    template: `<button 
+        v-bind:title="title" v-bind:class="'icon-button icon-button-'+position+' icon-button-'+type">
+        <img v-bind:src="icon"/>
+        <content><slot></slot></content>
+        </button>`
+}).mount('#app')
